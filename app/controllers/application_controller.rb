f741948e35cfdfb53c14869897eccf3cc5d08b4b
nanaasiedu-ansah <sirnana1@gmail.com>
2019-05-21 15:15:00 -0400

    Added admnin user to user table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,7 @@ end
 
   end
 
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_024751) do
+ActiveRecord::Schema.define(version: 2019_05_21_172259) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2019_05_16_024751) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
